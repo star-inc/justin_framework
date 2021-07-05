63890ce8c95d7c4ec8ae5768d9d4e39555a9023a
SuperSonic <supersonic@livemail.tw>
2021-07-05 20:46:43 +0800

    first commit

---
+++ Controllers/ControllerInterface.php
@@ -0,0 +1,16 @@
+<?php
+// Justin PHP Framework
+// (c)2021 SuperSonic(https://randychen.tk)
+
+interface ControllerInterface
+{
+    public function getRequest(): Request;
+
+    public function getResponse(): Response;
+
+    public function getConfig(): Config;
+
+    public function getDatabase(): Database;
+
+    public function trigger(): void;
+}
